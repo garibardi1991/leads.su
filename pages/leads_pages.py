2b6e8145f3d95eae2b2db73c3ee14b5bf1db61ac
igor trubikhov <trubikhov.i@leads.su>
2024-07-28 18:02:36 +0300

    add code

--- pages/leads_pages.py
+++ pages/leads_pages.py
@@ -1,14 +1,23 @@
 from selene import browser, have
 import time
+import os
+from dotenv import load_dotenv
+
+
+def load_env():
+    load_dotenv()
 
 
 class AuthenticationForm:
+
     def open(self):
         browser.open('login')
 
     def type_log_pass(self):
-        browser.element('#webmaster_models_web_LoginForm_email').type('trubikhov.i@leads.su')
-        browser.element('#webmaster_models_web_LoginForm_password').type('Igor25041991!')
+        login = os.getenv('LOG')
+        password = os.getenv('PASS')
+        browser.element('#webmaster_models_web_LoginForm_email').type(login)
+        browser.element('#webmaster_models_web_LoginForm_password').type(password)
         button = browser.element('[type=submit]')
         button.click()
 
