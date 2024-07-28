67169569e4a519af631871da40072a1c03ef2c33
igor trubikhov <trubikhov.i@leads.su>
2024-07-28 15:28:20 +0300

    add code

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ from selenium.webdriver.chrome.options import Options
 from selene import browser
 from utils import attach
 from dotenv import load_dotenv
+from pages.leads_pages import AuthenticationForm
 
 DEFAULT_BROWSER_VERSION = "100.0"
 
@@ -49,6 +50,45 @@ def setup_browser(request):
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.config.driver = driver
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
+    browser.quit()
+
+
+@pytest.fixture(scope='function')
+def setup_browser_auto(request):
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+    browser.config.base_url = 'https://webmaster.leads.su/'
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    browser.config.driver = driver
+    authentication_form = AuthenticationForm()
+    authentication_form.open()
+    authentication_form.type_log_pass()
     yield browser
 
     attach.add_screenshot(browser)
