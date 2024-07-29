a176231a788f8ccf78cf68bdf69093cb044abd36
igor trubikhov <trubikhov.i@leads.su>
2024-07-29 23:50:02 +0300

    add code

--- models/pages/ui/loading_avatar_page.py
+++ models/pages/ui/loading_avatar_page.py
@@ -1,6 +1,5 @@
-from importlib import resources
-
 import allure
+import requests
 from selene import browser
 
 from utils import resource
@@ -16,7 +15,10 @@ class LoadingAvatar:
             browser.element('[type="file"]').set_value(resource.path(file))
 
     def checking_file(self):
-        with allure.step("Проверяем загруженное изаброжение"):
+        with allure.step("Проверяем загруженное изображение"):
+            with open('photo_2022-08-18_21-18-12.jpg', "wb") as file:
+                response = requests.get('https://logo.s3.leads.su//197686/127170/photo_2022-08-18_21-18-12.jpg')
+                file.write(response.content)
 
     def click_save_button(self):
         with allure.step("Сохрание профиля"):
