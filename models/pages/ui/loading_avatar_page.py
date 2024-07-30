ebab4cb1a7db0b04e1074a8f0fd6527119ebb8f7
igor trubikhov <trubikhov.i@leads.su>
2024-07-30 09:36:47 +0300

    add code

--- models/pages/ui/loading_avatar_page.py
+++ models/pages/ui/loading_avatar_page.py
@@ -14,11 +14,11 @@ class LoadingAvatar:
         with allure.step("Загрузка изображения"):
             browser.element('[type="file"]').set_value(resource.path(file))
 
-    def checking_file(self):
-        with allure.step("Проверяем загруженное изображение"):
-            with open('photo_2022-08-18_21-18-12.jpg', "wb") as file:
-                response = requests.get('https://logo.s3.leads.su//197686/127170/photo_2022-08-18_21-18-12.jpg')
-                file.write(response.content)
+    # def checking_file(self):
+    #     with allure.step("Проверяем загруженное изображение"):
+    #         with open('photo_2022-08-18_21-18-12.jpg', "wb") as file:
+    #             response = requests.get('https://logo.s3.leads.su//197686/127170/photo_2022-08-18_21-18-12.jpg')
+    #             file.write(response.content)
 
     def click_save_button(self):
         with allure.step("Сохрание профиля"):
