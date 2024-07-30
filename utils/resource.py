24b4351ab86d38723352952d0ef7ee69dec63763
igor trubikhov <trubikhov.i@leads.su>
2024-07-30 15:08:24 +0300

    add code

--- utils/resource.py
+++ utils/resource.py
@@ -3,6 +3,6 @@ from pathlib import Path
 
 def path(file_name):
     return str(
-        Path(__file__).parent.parent.parent.parent.joinpath(
+        Path(__file__).parent.parent.joinpath(
             f'resources/{file_name}')
     )
