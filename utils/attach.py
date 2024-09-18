3cb33144d1f5d1e6d63207e8a7cebf673cd76f15
igor trubikhov <trubikhov.i@leads.su>
2024-09-18 15:20:35 +0300

    add code

--- utils/attach.py
+++ utils/attach.py
@@ -1,6 +1,9 @@
 import allure
-from allure_commons.types import AttachmentType
+import json
+import logging
 
+from allure_commons.types import AttachmentType
+from requests import Response
 
 def add_screenshot(browser):
     png = browser.driver.get_screenshot_as_png()
@@ -23,3 +26,50 @@ def add_video(browser):
            + video_url \
            + "' type='video/mp4'></video></body></html>"
     allure.attach(html, 'video_' + browser.driver.session_id, AttachmentType.HTML, '.html')
+
+
+def response_logging(response: Response):
+    logging.info("Request: " + response.request.url)
+    if response.request.body:
+        logging.info("INFO Request body: " + response.request.body)
+    logging.info("Request headers: " + str(response.request.headers))
+    logging.info("Response code " + str(response.status_code))
+    logging.info("Response: " + response.text)
+
+
+def response_attaching(response: Response):
+
+    allure.attach(
+        body=response.request.url,
+        name="Request url",
+        attachment_type=AttachmentType.TEXT,
+    )
+    allure.attach(
+        body=response.request.method,
+        name="Method",
+        attachment_type=AttachmentType.TEXT,
+    )
+    allure.attach(
+        body=str(response.request.headers),
+        name="Request headers",
+        attachment_type=AttachmentType.TEXT,
+    )
+    allure.attach(
+        body=str(response.status_code),
+        name="Response code",
+        attachment_type=AttachmentType.TEXT,
+    )
+
+    if response.request.body:
+        allure.attach(
+            body=json.dumps(response.request.body, indent=4, ensure_ascii=True),
+            name="Request body",
+            attachment_type=AttachmentType.JSON,
+            extension="json",
+        )
+        allure.attach(
+            body=json.dumps(response.json(), indent=4, ensure_ascii=True),
+            name="Response",
+            attachment_type=AttachmentType.JSON,
+            extension="json",
+        )
