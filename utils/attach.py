f72591554afe7a2066b7d71826e52bf69a4f09e9
igor trubikhov <trubikhov.i@leads.su>
2024-09-18 15:32:56 +0300

    add code

--- utils/attach.py
+++ utils/attach.py
@@ -3,6 +3,7 @@ import json
 import logging
 
 from allure_commons.types import AttachmentType
+from curlify import to_curl
 from requests import Response
 
 def add_screenshot(browser):
@@ -30,39 +31,34 @@ def add_video(browser):
 
 def response_logging(response: Response):
     logging.info("Request: " + response.request.url)
+
     if response.request.body:
-        logging.info("INFO Request body: " + response.request.body)
+        request_body = response.request.body.decode('utf-8')
+        logging.info("INFO Request body: " + request_body)
+
     logging.info("Request headers: " + str(response.request.headers))
-    logging.info("Response code " + str(response.status_code))
+    logging.info("Response code: " + str(response.status_code))
     logging.info("Response: " + response.text)
 
 
-def response_attaching(response: Response):
+def request_attaching(response: Response):
+    with allure.step(f'{response.request.method} {response.request.url}'):
+        curl = to_curl(response.request)
+        logging.info(curl)
+        logging.info(f'status code: {response.status_code}')
+        allure.attach(body=curl, name='curl', attachment_type=allure.attachment_type.TEXT, extension='txt')
 
+
+def response_attaching(response: Response):
     allure.attach(
         body=response.request.url,
         name="Request url",
         attachment_type=AttachmentType.TEXT,
     )
-    allure.attach(
-        body=response.request.method,
-        name="Method",
-        attachment_type=AttachmentType.TEXT,
-    )
-    allure.attach(
-        body=str(response.request.headers),
-        name="Request headers",
-        attachment_type=AttachmentType.TEXT,
-    )
-    allure.attach(
-        body=str(response.status_code),
-        name="Response code",
-        attachment_type=AttachmentType.TEXT,
-    )
 
     if response.request.body:
         allure.attach(
-            body=json.dumps(response.request.body, indent=4, ensure_ascii=True),
+            body=response.request.body.decode('utf-8') if response.request.body else None,
             name="Request body",
             attachment_type=AttachmentType.JSON,
             extension="json",
