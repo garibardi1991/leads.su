7800251abd5fe158a7d8faa588ffd2b62bc33c30
igor trubikhov <trubikhov.i@leads.su>
2024-09-19 10:46:19 +0300

    add code

--- tests/api/test_demo_qa.py
+++ tests/api/test_demo_qa.py
@@ -42,7 +42,11 @@ def test_no_authorization_user(base_url):
     with allure.step('Проверяем что статус кода == 200'):
         assert response.status_code == 200
     with allure.step('Проверяем, что вернулся пустой текст, подтверждающий некорректную авторизацию'):
-        assert response.text == 'Null'
+        try:
+            json_response = response.json()
+            assert json_response is None
+        except ValueError:
+            assert response.text == 'Null'
 
 
 @allure.tag("API")
